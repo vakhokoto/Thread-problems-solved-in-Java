accc67132e4674f2f82ca3e3e67f9c1c0a32face
vakhokoto <v.kotoreishvili@gmail.com>
2019-05-15 02:50:02 +0400

    Web ნაწილი არ არის დასრულებული გრაფიკა ჭედავს

--- WebLoaderFrame.java
+++ WebLoaderFrame.java
@@ -7,10 +7,12 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.TreeSet;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Semaphore;
 
 public class WebLoaderFrame extends JPanel {
 
@@ -18,12 +20,12 @@ public class WebLoaderFrame extends JPanel {
     private JPanel panel;
     private DefaultTableModel model;
     private JTable table;
-    private static JProgressBar progressBar;
+    private JProgressBar progressBar;
     private JButton oneThreadB, multyThreadB, stop;
     private JTextField numberField;
-    private int completedNumber, runNumber, startTIme, endTime;
     private ArrayList<String> list;
     private Thread[] threads;
+    private static Thread listenerThread = null;
 
     private static final int MAX_THREADS = 4;
     private static final int FIELD_SIZE = 5;
@@ -34,10 +36,9 @@ public class WebLoaderFrame extends JPanel {
     private static final String FILE_NAME = "links.txt";
 
     public WebLoaderFrame(){
-        runNumber = startTIme = endTime = completedNumber = 0;
-        running = new JLabel("Running: 0");
-        completed = new JLabel("Completed: 0");
-        elapsed = new JLabel("Elapsed: 0");
+        running = new JLabel("0");
+        completed = new JLabel("0");
+        elapsed = new JLabel("0");
         oneThreadB = new JButton("Single Thread Fetch");
         multyThreadB = new JButton("Concurent Fetch");
         stop = new JButton("Stop");
@@ -60,11 +61,11 @@ public class WebLoaderFrame extends JPanel {
         add(Box.createRigidArea(new Dimension(0,5)));
         add(numberField);
         add(Box.createRigidArea(new Dimension(0,5)));
-        add(running);
+        addLabel("Running: ", running);
         add(Box.createRigidArea(new Dimension(0,5)));
-        add(completed);
+        addLabel("Completed: ", completed);
         add(Box.createRigidArea(new Dimension(0,5)));
-        add(elapsed);
+        addLabel("Elapsed: ", elapsed);
         add(Box.createRigidArea(new Dimension(0,5)));
         add(progressBar);
         add(Box.createRigidArea(new Dimension(0,5)));
@@ -76,6 +77,21 @@ public class WebLoaderFrame extends JPanel {
     }
 
     /**
+     * This method adds labels so as to be in the middle and also
+     * number and text to be separated
+     *
+     * @param s string to which label to be followed
+     * @param label JLabel which should ne added next to JLabel
+     * */
+    private void addLabel(String s, JLabel label) {
+        JPanel helperPanel = new JPanel();
+        helperPanel.setLayout(new FlowLayout());
+        helperPanel.add(new JLabel(s));
+        helperPanel.add(label);
+        add(helperPanel);
+    }
+
+    /**
      * This method adds listeners of the buttons
      * */
     private void addListeners() {
@@ -84,31 +100,56 @@ public class WebLoaderFrame extends JPanel {
             public void actionPerformed(ActionEvent e) {
                 oneThreadB.setEnabled(false);
                 multyThreadB.setEnabled(false);
+                listenerThread = Thread.currentThread();
+                long startTime = System.currentTimeMillis();
+                for (int i=0; i<list.size(); ++i){
+                    model.setValueAt("", i, 1);
+                }
                 stop.setEnabled(true);
                 makeThreads(1);
+                long endTime = System.currentTimeMillis();
+                elapsed.setText("" + ((double)(endTime - startTime) / 1000) + " seconds");
+                oneThreadB.setEnabled(true);
+                multyThreadB.setEnabled(true);
+                stop.setEnabled(false);
+                progressBar.setValue(0);
             }
         });
 
         multyThreadB.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                oneThreadB.setEnabled(false);
+                multyThreadB.setEnabled(false);
+                listenerThread = Thread.currentThread();
+                long startTime = System.currentTimeMillis();
                 String s = numberField.getText();
                 if (!check(s)){
                     return;
                 }
-                oneThreadB.setEnabled(false);
-                multyThreadB.setEnabled(false);
                 stop.setEnabled(true);
+                for (int i=0; i<list.size(); ++i){
+                    model.setValueAt("", i, 1);
+                }
                 makeThreads(Integer.parseInt("" + s.charAt(0)));
+                long endTime = System.currentTimeMillis();
+                elapsed.setText("" + ((double)(endTime - startTime) / 1000.0) + " seconds");
+                progressBar.setValue(0);
+                oneThreadB.setEnabled(true);
+                multyThreadB.setEnabled(true);
+                stop.setEnabled(false);
             }
         });
 
         stop.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                oneThreadB.setEnabled(true);
-                multyThreadB.setEnabled(true);
-                stop.setEnabled(false);
+                listenerThread.interrupt();
+                try {
+                    listenerThread.join();
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
             }
         });
     }
@@ -124,17 +165,52 @@ public class WebLoaderFrame extends JPanel {
                 || s.length() == 1 && Integer.parseInt("" + s.charAt(0)) > MAX_THREADS);
     }
 
-    private static final int QUEUE_SIZE = 10;
+    private static final int QUEUE_SIZE = 4;
 
     /**
      * This method creates and runs worker threads
+     *
+     * @param n the maximum number of thread which  could be running simultaneously
      * */
     private void makeThreads(int n) {
-        threads = new Thread[n];
-        BlockingQueue<String> queue = new ArrayBlockingQueue<>(QUEUE_SIZE);
-        for (int i=0; i<n; ++i){
-            threads[i] = new WebWorker(queue);
-            threads[i].start();
+        Semaphore lock = new Semaphore(n);
+        int size = list.size();
+        WebWorker[] workers = new WebWorker[size];
+        int pos = 0;
+        boolean ind = false;
+        for (pos=0; pos<size; pos++){
+            workers[pos] = new WebWorker(list.get(pos), model, pos, 1, running, completed, lock, progressBar);
+            if (listenerThread.isInterrupted()){
+                ind = true;
+                break;
+            }
+            try {
+                lock.acquire();
+            } catch (InterruptedException e) {
+                ind = true;
+                break;
+            }
+            workers[pos].start();
+        }
+        for (int i=0; i<=Math.min(pos, list.size() - 1); ++i){
+            try {
+                workers[i].join();
+            } catch (InterruptedException e) {
+                ind = true;
+                break;
+            }
+        }
+        if (ind){
+            for (int i=0; i<=pos; i++){
+                workers[i].interrupt();
+            }
+            for (int i=0; i<=pos; ++i){
+                try {
+                    workers[i].join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
--- WebWorker.java
+++ WebWorker.java
@@ -1,67 +1,125 @@
 import java.io.*;
 import java.net.*;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Semaphore;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class WebWorker extends Thread {
 
-    private BlockingQueue<String> queue;
+    private DefaultTableModel model;
+    private String urlString;
+    private int x, y;
+    private Semaphore callThatEnded;
+    private JLabel running, completed;
+    private JProgressBar bar;
 
-    public WebWorker(BlockingQueue<String> queue){
-        this.queue = queue;
+    public WebWorker(String urlString, DefaultTableModel model, int x, int y, JLabel running, JLabel completed, Semaphore callThatEnded, JProgressBar bar){
+        this.urlString = urlString;
+        this.model = model;
+        this.x = x;
+        this.y = y;
+        this.callThatEnded = callThatEnded;
+        this.running = running;
+        this.completed = completed;
+        this.bar = bar;
     }
 
     @Override
-    public void run(){
-        System.out.println("yle");
+    public void run() {
+        updateLabel(running,1);
+        InputStream input = null;
+        StringBuilder contents = null;
+        try {
+            long startTime, endTime;
+            startTime = System.currentTimeMillis();
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+
+            // Set connect() to throw an IOException
+            // if connection does not succeed in this many msecs.
+            connection.setConnectTimeout(5000);
+
+            connection.connect();
+            input = connection.getInputStream();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            char[] array = new char[1000];
+            long sizeOfFileDownloaded = 0;
+            int len;
+            contents = new StringBuilder(1000);
+            boolean ind = false;
+            while ((len = reader.read(array, 0, array.length)) > 0) {
+                if (isInterrupted()){
+                    ind = true;
+                    setModelValue("interrupted");
+                    break;
+                }
+                contents.append(array, 0, len);
+                Thread.sleep(100);
+                sizeOfFileDownloaded += len * 2;
+            }
+            if (!ind) {
+                endTime = System.currentTimeMillis();
+                Date date = new Date();
+                String strDateFormat = "HH:mm:ss";
+                DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
+                String formattedDate = dateFormat.format(date);
+                setModelValue("" + formattedDate + " " + (endTime - startTime) + "ms " + sizeOfFileDownloaded + " bytes");
+            }
+        }
+        // Otherwise control jumps to a catch...
+        catch (MalformedURLException ignored) {
+            setModelValue("err");
+        } catch (InterruptedException exception) {
+            setModelValue("interrupted");
+        } catch (IOException ignored) {
+            setModelValue("err");
+        }
+        // "finally" clause, to close the input stream
+        // in any case
+        finally {
+            try {
+                if (input != null) input.close();
+            } catch (IOException ignored) {
+                setModelValue("err");
+            }
+        }
+
+        updateLabel(running,-1);
+        updateLabel(completed, 1);
+        callThatEnded.release();
+        synchronized (bar) {
+            bar.setValue(bar.getValue() + 1);
+        }
     }
 
-/*
-  This is the core web/download i/o code...
- 		InputStream input = null;
-		StringBuilder contents = null;
-		try {
-			URL url = new URL(urlString);
-			URLConnection connection = url.openConnection();
-		
-			// Set connect() to throw an IOException
-			// if connection does not succeed in this many msecs.
-			connection.setConnectTimeout(5000);
-			
-			connection.connect();
-			input = connection.getInputStream();
+    /**
+     * This method sets value of model grid at str
+     *
+     * @param str string to which grid should be set
+     * */
+    private void setModelValue(String str) {
+        synchronized (model){
+            model.setValueAt(str, x, y);
+        }
+    }
 
-			BufferedReader reader  = new BufferedReader(new InputStreamReader(input));
-		
-			char[] array = new char[1000];
-			int len;
-			contents = new StringBuilder(1000);
-			while ((len = reader.read(array, 0, array.length)) > 0) {
-				contents.append(array, 0, len);
-				Thread.sleep(100);
-			}
-			
-			// Successful download if we get here
-			
-		}
-		// Otherwise control jumps to a catch...
-		catch(MalformedURLException ignored) {}
-		catch(InterruptedException exception) {
-			// YOUR CODE HERE
-			// deal with interruption
-		}
-		catch(IOException ignored) {}
-		// "finally" clause, to close the input stream
-		// in any case
-		finally {
-			try{
-				if (input != null) input.close();
-			}
-			catch(IOException ignored) {}
-		}
-
-*/
-	
+    /**
+     * This mwthod updates value of the specified JLabel by pl
+     *
+     * @param label label to be updated
+     * @param pl integet by which value should be updated
+     * */
+    private void updateLabel(JLabel label, int pl) {
+        synchronized (label) {
+            String val = label.getText();
+            int next = Integer.parseInt(val) + pl;
+            label.setText("" + next);
+        }
+    }
 }
